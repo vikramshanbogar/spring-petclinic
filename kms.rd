3821ae0aa58fb020c0f511678380f08fc8b412c7
Vikram Shanbogar <70604290+vikramshanbogar@users.noreply.github.com>
2025-11-17 11:06:28 +0530

    Revert "Added KMS"
    This reverts commit d2a7e41a36d2311a5bbc5ac94aab4d2a3fd58e73.

--- kms.rd
+++
@@ -1,6 +0,0 @@
-
-$ echo -n petclinic | base64
-cGV0Y2xpbmlj
-
-
-$ aws kms encrypt   --key-id 76e14095-03c6-494f-b7f3-8550cd902184   --plaintext "cGV0Y2xpbmlj"   --output text   --query CiphertextBlob
--- src/main/java/org/springframework/samples/petclinic/Controller/HelloController.java
+++ src/main/java/org/springframework/samples/petclinic/Controller/HelloController.java
@@ -11,24 +11,24 @@ import java.util.Map;
 @RequestMapping
 public class HelloController {
 
-	@GetMapping("/hello")
-	Map<String, String> sayHello() {
+    @GetMapping("/hello")
+    Map<String, String> sayHello() {
 
-		// Get current size of heap in bytes.
-		long heapSize = Runtime.getRuntime().totalMemory();
+        // Get current size of heap in bytes.
+        long heapSize = Runtime.getRuntime().totalMemory();
 
-		// Get maximum size of heap in bytes. The heap cannot grow beyond this size.
-		// Any attempt will result in an OutOfMemoryException.
-		long heapMaxSize = Runtime.getRuntime().maxMemory();
+// Get maximum size of heap in bytes. The heap cannot grow beyond this size.
+// Any attempt will result in an OutOfMemoryException.
+        long heapMaxSize = Runtime.getRuntime().maxMemory();
 
-		// Get amount of free memory within the heap in bytes. This size will
-		// increase after garbage collection and decrease as new objects are created.
-		long heapFreeSize = Runtime.getRuntime().freeMemory();
-		Map<String, String> map = new HashMap<>();
-		map.put("heapSize:- ", String.valueOf((heapSize / 1024) / 1024));
-		map.put("heapMaxSize:- ", String.valueOf((heapMaxSize / 1024) / 1024));
-		map.put("heapFreeSize:- ", String.valueOf((heapFreeSize / 1024) / 1024));
-		return map;
-	}
+// Get amount of free memory within the heap in bytes. This size will
+// increase after garbage collection and decrease as new objects are created.
+        long heapFreeSize = Runtime.getRuntime().freeMemory();
+        Map<String, String> map = new HashMap<>();
+        map.put("heapSize:- " , String.valueOf((heapSize/ 1024)/1024));
+        map.put("heapMaxSize:- " , String.valueOf((heapMaxSize/ 1024)/1024));
+        map.put("heapFreeSize:- " , String.valueOf((heapFreeSize/ 1024)/1024));
+        return map;
+    }
 
 }
